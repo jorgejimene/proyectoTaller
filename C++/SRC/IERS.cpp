d147a273ceac0aa2e31a6f29d6243b5b3608aded
jorgejimene <113776889+jorgejimene@users.noreply.github.com>
2025-05-14 17:13:20 +0200

    funciona IERS

--- C++/SRC/IERS.cpp
+++ C++/SRC/IERS.cpp
@@ -10,7 +10,7 @@
 %
 %--------------------------------------------------------------------------
 */
-void IERS(const Matrix& eop, double Mjd_UTC,  double x_pole, double y_pole, double UT1_UTC, double LOD, double dpsi, double deps, double dx_pole, double dy_pole, double TAI_UTC,char interp = 'o'){
+void IERS(Matrix &eop, double Mjd_UTC,  double &x_pole, double &y_pole, double &UT1_UTC, double &LOD, double &dpsi, double &deps, double &dx_pole, double &dy_pole, double &TAI_UTC,char interp = 'o'){
 
 
     if(interp == 'o') {
